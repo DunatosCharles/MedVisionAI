1af32feb94795da175cf7552e7b6f4d69b5189d1
DunatosCharles <102760333+DunatosCharles@users.noreply.github.com>
2026-07-08 18:45:43 +0800

    Add data augmentation experiment

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import DataLoader
 from medmnist import BreastMNIST
 from torchvision import transforms
@@ -6,31 +5,58 @@ from torchvision import transforms
 
 def get_dataloaders(batch_size=32):
 
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
+
+        transforms.RandomRotation(
+            degrees=10
+        ),
+
+        transforms.RandomAffine(
+            degrees=0,
+            translate=(0.1, 0.1),
+            scale=(0.9, 1.1)
+        ),
+
         transforms.ToTensor(),
+
         transforms.Normalize(
             mean=[0.5],
             std=[0.5]
         )
     ])
 
+
+    test_transform = transforms.Compose([
+
+        transforms.ToTensor(),
+
+        transforms.Normalize(
+            mean=[0.5],
+            std=[0.5]
+        )
+    ])
+
+
     train_dataset = BreastMNIST(
         split="train",
         download=True,
-        transform=transform
+        transform=train_transform
     )
+
 
     val_dataset = BreastMNIST(
         split="val",
         download=True,
-        transform=transform
+        transform=test_transform
     )
+
 
     test_dataset = BreastMNIST(
         split="test",
         download=True,
-        transform=transform
+        transform=test_transform
     )
+
 
     train_loader = DataLoader(
         train_dataset,
@@ -38,16 +64,19 @@ def get_dataloaders(batch_size=32):
         shuffle=True
     )
 
+
     val_loader = DataLoader(
         val_dataset,
         batch_size=batch_size,
         shuffle=False
     )
 
+
     test_loader = DataLoader(
         test_dataset,
         batch_size=batch_size,
         shuffle=False
     )
+
 
     return train_loader, val_loader, test_loader
